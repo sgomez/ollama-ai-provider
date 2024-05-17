9e52f673f734b7ed123862221dbdfa066c198e96
Sergio Gómez <decano@gmail.com>
2024-05-17 19:46:34 +0200

    feat: add stream-text examples
    Except:
    
    - ollama-chatbot-with-tools.ts
    - ollama-fullstream.ts

---
+++ examples/ai-core/src/stream-text/ollama-chatbot.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env -S pnpm tsx
+
+import * as readline from 'node:readline/promises'
+
+import { CoreMessage, streamText } from 'ai'
+import { ollama } from 'ollama-ai-provider'
+
+import { buildProgram } from '../tools/command'
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const messages: CoreMessage[] = []
+
+async function main(model: Parameters<typeof ollama>[0]) {
+  while (true) {
+    const userInput = await terminal.question('You: ')
+
+    messages.push({ content: userInput, role: 'user' })
+
+    const result = await streamText({
+      messages,
+      model: ollama(model),
+      system: `You are a helpful, respectful and honest assistant.`,
+    })
+
+    let fullResponse = ''
+    process.stdout.write('\nAssistant: ')
+    for await (const delta of result.textStream) {
+      fullResponse += delta
+      process.stdout.write(delta)
+    }
+    process.stdout.write('\n\n')
+
+    messages.push({ content: fullResponse, role: 'assistant' })
+  }
+}
+
+buildProgram('llama3', main).catch(console.error)
