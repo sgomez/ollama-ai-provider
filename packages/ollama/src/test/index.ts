92cb1dcf1b7b107842669b2ab3a67cb3db6a49fc
Sergio Gómez <decano@gmail.com>
2024-07-07 10:00:27 +0200

    test: split stream in lines

---
+++ packages/ollama/src/test/index.ts
@@ -0,0 +1,2 @@
+export * from './convert-array-to-readable-stream'
+export * from './convert-readable-stream-to-array'
---
+++ packages/ollama/src/utils/response-handler.test.ts
@@ -0,0 +1,73 @@
+import { z } from 'zod'
+
+import {
+  convertArrayToReadableStream,
+  convertReadableStreamToArray,
+} from '@/test'
+
+import { createJsonStreamResponseHandler } from './response-handler'
+
+describe('createJsonStreamResponseHandler', () => {
+  it('should return a stream of complete json chunks', async () => {
+    const handler = createJsonStreamResponseHandler(z.object({ a: z.number() }))
+
+    const { value: stream } = await handler({
+      requestBodyValues: {},
+      response: new Response(
+        convertArrayToReadableStream([
+          JSON.stringify({ a: 1 }) + '\n',
+          JSON.stringify({ a: 2 }) + '\n',
+        ]).pipeThrough(new TextEncoderStream()),
+      ),
+      url: 'some url',
+    })
+
+    expect(await convertReadableStreamToArray(stream)).toStrictEqual([
+      { success: true, value: { a: 1 } },
+      { success: true, value: { a: 2 } },
+    ])
+  })
+
+  it('should return a stream of partial json chunks', async () => {
+    const handler = createJsonStreamResponseHandler(z.object({ a: z.number() }))
+
+    const { value: stream } = await handler({
+      requestBodyValues: {},
+      response: new Response(
+        convertArrayToReadableStream([
+          '{ "a":', // start
+          '1 }\n', // end
+        ]).pipeThrough(new TextEncoderStream()),
+      ),
+      url: 'some url',
+    })
+
+    expect(await convertReadableStreamToArray(stream)).toStrictEqual([
+      { success: true, value: { a: 1 } },
+    ])
+  })
+
+  it('should return a stream of multiple json chunks', async () => {
+    const handler = createJsonStreamResponseHandler(z.object({ a: z.number() }))
+
+    const { value: stream } = await handler({
+      requestBodyValues: {},
+      response: new Response(
+        convertArrayToReadableStream([
+          JSON.stringify({ a: 1 }) + '\n' + JSON.stringify({ a: 2 }) + '\n',
+          '{ "a":', // start
+          '3 }\n', // end
+          JSON.stringify({ a: 4 }) + '\n',
+        ]).pipeThrough(new TextEncoderStream()),
+      ),
+      url: 'some url',
+    })
+
+    expect(await convertReadableStreamToArray(stream)).toStrictEqual([
+      { success: true, value: { a: 1 } },
+      { success: true, value: { a: 2 } },
+      { success: true, value: { a: 3 } },
+      { success: true, value: { a: 4 } },
+    ])
+  })
+})
