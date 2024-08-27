b298722e590b662cf74de2a3be4aa1c1a4324f0f
Sergio Gómez <decano@gmail.com>
2024-08-27 18:01:21 +0200

    feat: add customProvider examples

--- examples/ai-core/src/registry/setup-registry.ts
+++ examples/ai-core/src/registry/setup-registry.ts
@@ -1,7 +1,17 @@
-import { experimental_createProviderRegistry as createProviderRegistry } from 'ai'
+import {
+  experimental_createProviderRegistry as createProviderRegistry,
+  experimental_customProvider as customProvider,
+} from 'ai'
 import { ollama } from 'ollama-ai-provider'
 
+const myOllama = customProvider({
+  fallbackProvider: ollama,
+  languageModels: {
+    multimodal: ollama('llava'),
+    text: ollama('llama3.1'),
+  },
+})
+
 export const registry = createProviderRegistry({
-  // register provider with prefix and custom setup:
-  ollama,
+  ollama: myOllama,
 })
