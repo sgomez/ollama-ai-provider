a66de01cf2349bfd22532c45b5b70e952bacecb5
Ege Cavusoglu <egecavusoglu99@gmail.com>
2024-11-25 21:01:40 -0800

    Adds whitespace-pre-wrap to message

--- examples/weather-ollama/src/components/message.tsx
+++ examples/weather-ollama/src/components/message.tsx
@@ -17,7 +17,7 @@ export function Message({
       </div>
 
       <div className="flex flex-col gap-1">
-        <div className="text-zinc-800 dark:text-zinc-300 flex flex-col gap-4">
+        <div className="text-zinc-800 dark:text-zinc-300 flex flex-col gap-4 whitespace-pre-wrap">
           {children}
         </div>
       </div>
